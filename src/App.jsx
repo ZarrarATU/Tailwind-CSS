b3cb3162cc82a717b421c5adb018d2a7c9ae8a1d
ZarrarATU <zarrarantiterrorxd@gmail.com>
2025-12-23 03:35:50 -0800

    basic grid

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 function App() {
   return (
-    <div>App</div>
+       <div className='grid gap-4 m-4 sm:grid-cols-12 grid-cols-2'>
+        <div className='rounded-lg sm:col-span-4 bg-amber-800 shadow-xl min-h-[100px]'></div>
+        <div className='rounded-lg sm:col-span-6 bg-amber-950 shadow-xl min-h-[100px]'></div>
+        <div className='rounded-lg bg-amber-700 shadow-xl sm:col-span-2 min-h-[100px] hidden sm:block'></div>
+       </div>
   )
 }
 
