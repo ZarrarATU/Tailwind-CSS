811058c0fb7778cea1d5639a2caf7e18427a9060
ZarrarATU <zarrarantiterrorxd@gmail.com>
2025-12-27 00:54:06 -0800

    simple card

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,22 @@ import React from 'react'
 
 function App() {
   return (
-       <div className='grid gap-4 m-4 sm:grid-cols-12 grid-cols-2'>
-        <div className='rounded-lg sm:col-span-4 bg-amber-800 shadow-xl min-h-[100px]'></div>
-        <div className='rounded-lg sm:col-span-6 bg-amber-950 shadow-xl min-h-[100px]'></div>
-        <div className='rounded-lg bg-amber-700 shadow-xl sm:col-span-2 min-h-[100px] hidden sm:block'></div>
-       </div>
+    <div className="main grid place-content-center h-screen">
+      <h1 className='text-white text-4xl m-6'>TAILWIND CSS</h1>
+
+      <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-2xl text-black flex items-center">
+        <div className='mr-7'>
+          <img className='w-22' src="/pas.png" alt="logo" />
+        </div>
+        <div>
+          <div>
+            <h3 className='text-2xl font-bold'>Zarrar ATU</h3>
+            <p className='text-[14px] text-slate-600'>Zarrar is a private company that enhances Pakistan’s security</p>
+          </div>
+        </div>
+      </div>
+
+    </div>
   )
 }
 
